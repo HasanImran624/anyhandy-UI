97575cc7d1208627509cfd88548f4fcc9ebdf0ee
Muhammad Talha waqar <talhawaqar007@gmail.com>
2024-05-04 21:26:40 +0500

    reduce text size of services

---
+++ src/Constants/serviceConstants.js
@@ -0,0 +1,200 @@
+export const services = [
+  {
+    id: 1,
+    name: "Painting",
+    uid: "painting",
+    subServices: [
+      {
+        id: 1,
+        name: "Interior Home Painting",
+      },
+      {
+        id: 2,
+        name: "Exterior Home Painting",
+      },
+      {
+        id: 3,
+        name: "Door Painting",
+      },
+      {
+        id: 4,
+        name: "Cabinet Painting or Refinishing",
+      },
+      {
+        id: 5,
+        name: "Fence Painting",
+      },
+      {
+        id: 6,
+        name: "Pressure Washing",
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: "Plumbing",
+    uid: "plumbing",
+    subServices: [
+      {
+        id: 1,
+        name: "Fixing Leaks (pipe repairs)",
+      },
+      {
+        id: 2,
+        name: "Clearing Clogged Drains",
+      },
+      {
+        id: 3,
+        name: "Installing or Repairing Faucets, Toilets, Water Heaters",
+      },
+      {
+        id: 4,
+        name: "Others",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "HVAC",
+    uid: "hvac",
+    subServices: [
+      {
+        id: 1,
+        name: "Regular Maintenance of Heating and Cooling Systems",
+      },
+      {
+        id: 2,
+        name: "Repairing or Replacing HVAC Units",
+      },
+      {
+        id: 3,
+        name: "Cleaning and Servicing Air Ducts",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Electrical Service",
+    uid: "electrical",
+    subServices: [
+      {
+        id: 1,
+        name: "Fixing General Electrical Issues",
+      },
+      {
+        id: 2,
+        name: "Installing or Repairing Light Fixtures and Ceiling Fans",
+      },
+      {
+        id: 3,
+        name: "Upgrading Electrical Panels",
+      },
+      {
+        id: 4,
+        name: "Mounting TVs and Home Theater Systems",
+      },
+      {
+        id: 5,
+        name: "Wiring for New Construction or Renovations",
+      },
+      {
+        id: 6,
+        name: "Electrical Troubleshooting",
+      },
+      {
+        id: 7,
+        name: "Others",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "General Handyman Services",
+    uid: "general_handyman_services",
+    subServices: [
+      {
+        id: 1,
+        name: "Miscellaneous Repairs and Maintenance Tasks",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Carpentry Services",
+    uid: "carpentry_services",
+    subServices: [
+      {
+        id: 1,
+        name: "Installing Shelves, Cabinets or Built-in Furniture",
+      },
+      {
+        id: 2,
+        name: "Fixing Doors and Windows",
+      },
+      {
+        id: 3,
+        name: "Others",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Home Cleaning Services",
+    uid: "home_cleaning_services",
+    subServices: [
+      {
+        id: 1,
+        name: "Regular Cleaning and Maintenance of the Home (area/hour)",
+      },
+      {
+        id: 2,
+        name: "Deep cleaning, Including Carpet Cleaning and Upholstery Cleaning",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Pest Control",
+    uid: "pest_control",
+    subServices: [
+      {
+        id: 1,
+        name: "Exterminating Pests such as Termites, Ants or Rodents",
+      },
+      {
+        id: 2,
+        name: "Preventive Pest Control Measures",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Landscaping And Lawn Care",
+    uid: "landscaping_and_lawn_care",
+    subServices: [
+      {
+        id: 1,
+        name: "Mowing the Lawn",
+      },
+      {
+        id: 2,
+        name: "Trimming Trees and Shrubs",
+      },
+      {
+        id: 3,
+        name: "Installing or Repairing Irrigation Systems",
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "Appliance Repairs",
+    uid: "appliances_repairs",
+    subServices: [
+      {
+        id: 1,
+        name: "Fixing or Servicing Household Appliances",
+      },
+    ],
+  },
+];
