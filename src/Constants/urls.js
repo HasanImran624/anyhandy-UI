c23821afc8a92ed8185cf01506a54d8874d82a06
Muhammad Talha waqar <talhawaqar007@gmail.com>
2024-05-14 04:56:22 +0500

    change urls

--- src/Constants/urls.js
+++ src/Constants/urls.js
@@ -1,4 +1,4 @@
 export const LOGIN_URL = "/user/validate-login";
 export const SIGNUP_URL = "/user/Sign-Up";
-export const SUBMIT_JOB_REQUEST_URL = "api/job/submit-job-post";
-export const GET_LIST_COUNTRIES_CITIES = "/api/country/cities-info";
+export const SUBMIT_JOB_REQUEST_URL = "/job/submit-job-post";
+export const GET_LIST_COUNTRIES_CITIES = "/country/cities-info";
